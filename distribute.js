3386940317b32be69ce0084cd262621a328711f3
levipaulk <levipaulk@gmail.com>
2019-06-18 14:15:02 -0400

    after afternoon session pt 2

--- distribute.js
+++ distribute.js
@@ -5,15 +5,8 @@ function distribute(arr){
   if(arr.length === 1){
     return new BinarySearchTree(arr[0])
   }
-  // is the length of the arr odd or even?
   // find the middle index
-  let middle = null
-  if(arr.length % 2 === 0){
-    middle = (arr.length / 2) + 0.5
-  }
-  else{
-    middle = arr.length / 2
-  }
+  let middle = Math.floor(arr.length / 2)
   // create a new tree
   let tree = new BinarySearchTree(arr[middle])
   // break arr in two halves
